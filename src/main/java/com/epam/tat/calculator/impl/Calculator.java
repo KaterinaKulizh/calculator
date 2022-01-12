39dc34aeef3b24eaafb09d6b5389b4042aa0b54f
KaterinaKulizh <sa7811036@yandex.ru>
2022-01-12 08:03:03 +0300

    calculator changes

--- src/main/java/com/epam/tat/calculator/impl/Calculator.java
+++ src/main/java/com/epam/tat/calculator/impl/Calculator.java
@@ -2,31 +2,45 @@ package com.epam.tat.calculator.impl;
 
 import com.epam.tat.calculator.ICalculator;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
 public class Calculator implements ICalculator {
-
     private int precision;
-
     public Calculator(int precision) {
         this.precision = precision;
     }
-
     @Override
     public double add(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        return BigDecimal.valueOf(a + b)
+                .setScale(precision, RoundingMode.HALF_UP)
+                .doubleValue();
     }
-
     @Override
     public double subtract(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
-    }
+        return BigDecimal.valueOf(a-b)
+                .setScale(precision, RoundingMode.HALF_UP)
+                .doubleValue();
 
+    }
     @Override
     public double multiply(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
-    }
-
+      return BigDecimal.valueOf(a*b)
+                .setScale(precision, RoundingMode.HALF_UP)
+                .doubleValue();
+           }
     @Override
     public double divide(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
-    }
+        if (a > 0 && b == 0) {
+            return Double.POSITIVE_INFINITY;
+        } else if (a < 0 && b == 0) {
+            return Double.NEGATIVE_INFINITY;
+        } else if (a == 0 && b == 0) {
+            return Double.NaN;
+        }
+        return BigDecimal.valueOf(a/b)
+                .setScale(precision, RoundingMode.HALF_UP)
+                .doubleValue();
+            }
 }
